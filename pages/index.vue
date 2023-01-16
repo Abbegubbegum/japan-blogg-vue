2774ee5467c5bea429dc4ce6a57bdb1826ef4ada
Albin Norback <albin.nojback@gmail.com>
2023-01-16 19:10:54 +0900

    Restructure and reformat

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { BlogPost } from '~/types/types.js'
+import { ref } from "vue";
+import { BlogPost } from "~/types/types.js";
 
-const blogs = ref<BlogPost[]>([])
+const blogs = ref<BlogPost[]>([]);
 
-fetch('/api/blogs')
+fetch("/api/blogs")
   .then((response) => response.json())
   .then((data: BlogPost[]) => {
-    blogs.value = data
+    blogs.value = data;
 
-    blogs.value.sort((a, b) => b.timestamp - a.timestamp)
-  })
+    blogs.value.sort((a, b) => b.timestamp - a.timestamp);
+  });
 
 function redirectToIndividual(timestamp: number) {
-  localStorage.setItem('timestamp', timestamp.toString())
-  window.location.pathname = '/individual.html'
+  localStorage.setItem("timestamp", timestamp.toString());
+  window.location.pathname = "/individual.html";
 }
 </script>
 
--- types/types.ts
+++ types/types.ts
@@ -1,18 +1,18 @@
 export type BlogPost = {
-  timestamp: number
-  author: string
-  title: string
-  location: string
-  date: string
-  content: (Paragraph | Image)[]
-}
+  timestamp: number;
+  author: string;
+  title: string;
+  location: string;
+  date: string;
+  content: (Paragraph | Image)[];
+};
 
 export type Paragraph = {
-  title: string
-  text: string
-}
+  title: string;
+  text: string;
+};
 
 export type Image = {
-  path: string
-  text: string
-}
+  path: string;
+  text: string;
+};
