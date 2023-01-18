e1233b85b400c40781936b71fa85c65aacab8f8d
Markus Piotrowski <Markuspiotrowski@live.com>
2023-01-18 13:14:16 +0900

    Imgs-Folder

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@ function redirectToIndividual(timestamp: number) {
 </script>
 
 <template>
-  <div>
+  <div id="blogContainer" class="content">
     <div
       class="blog-item"
       v-for="blog in blogs"
@@ -41,4 +41,123 @@ function redirectToIndividual(timestamp: number) {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  flex-direction: column;
+  background-color: antiquewhite;
+}
+.back-button {
+  align-self: flex-start;
+  margin: 1rem;
+  margin-left: 2rem;
+}
+
+#blogContainer {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 95%;
+  align-self: center;
+}
+
+.blogItem {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border: 2px solid black;
+  border-radius: 1rem;
+  margin: 1rem 0;
+}
+
+.blogItem:hover {
+  cursor: pointer;
+  background-color: #eee;
+}
+
+.blog-title h2 {
+  font-size: 5rem;
+  font-weight: 700;
+  letter-spacing: 0.1rem;
+}
+
+.blog-title {
+  margin: 2rem;
+}
+
+.blog-subheading {
+  margin-top: 0.7rem;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.blog-author {
+  font-size: 1.3rem;
+  font-weight: bold;
+}
+
+.blog-location {
+  font-size: 1.3rem;
+  letter-spacing: 0rem;
+  margin-left: 0.3rem;
+}
+
+.location-div {
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+}
+
+.blog-date {
+  position: absolute;
+  right: 1rem;
+  top: 0.8rem;
+  font-size: 1.4rem;
+}
+
+.blog-content {
+  margin: 2rem;
+  margin-top: 0;
+}
+
+.blog-content > div {
+  margin-bottom: 3rem;
+}
+
+.paragraf-title {
+  font-size: 3rem;
+  font-weight: lighter;
+}
+
+.paragraf-text {
+  max-width: 80ch;
+  font-size: 1.5rem;
+}
+
+.blog-img {
+  max-width: 60rem;
+  max-height: 30rem;
+}
+
+.img-text {
+  font-size: 1.2rem;
+}
+
+#aboutContainer {
+  display: flex;
+  font-size: xx-large;
+  width: 95%;
+  align-self: center;
+}
+</style>
